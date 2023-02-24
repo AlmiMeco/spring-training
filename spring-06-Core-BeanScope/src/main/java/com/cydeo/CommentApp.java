8ca4deb841590c2c634968134fc13ad2c667300a
Almi's PC <almimeci@gmail.com>
2023-02-24 17:30:10 -0600

    Bean Scope

---
+++ spring-06-Core-BeanScope/src/main/java/com/cydeo/CommentApp.java
@@ -0,0 +1,34 @@
+package com.cydeo;
+
+import com.cydeo.config.CommentConfig;
+import com.cydeo.entity.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CommentApp {
+
+
+    public static void main(String[] args) {
+
+        Comment comment = new Comment();
+        comment.setAuthor("Java");
+        comment.setText("Wooden Spoon");
+
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(CommentConfig.class);
+
+        CommentService cs1 = context.getBean(CommentService.class);
+        CommentService cs2 = context.getBean(CommentService.class);
+
+        System.out.println(cs1 == cs2);
+//        --> true  (if : Scope = Singleton)
+//        --> false (if : Scope = Prototype)
+
+
+
+
+    }
+
+
+}
---
+++ spring-06-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,37 @@
+package com.cydeo.service;
+
+import com.cydeo.entity.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+//@Scope(BeanDefinition.SCOPE_SINGLETON) <-- default scope for Beans
+public class CommentService {
+
+//    'final' keyword will make sure you don't forget to create a Constructor (Implicit AutoWiring)
+    private final CommentRepository commentDataBase;
+    private final CommentNotificationProxy emailNotification;
+
+    public void publishComment(Comment comment){
+//    REQUIREMENTS:
+        
+        //save to DB
+        commentDataBase.storeComment(comment);
+        //send as email
+        emailNotification.sendComment(comment);
+
+    }
+
+//    @Autowiring (Dependency Injection) done implicitly
+    @Autowired
+    public CommentService(CommentRepository commentDataBase,@Qualifier("emailCommentNotificationProxy") CommentNotificationProxy emailNotification) {
+        this.commentDataBase = commentDataBase;
+        this.emailNotification = emailNotification;
+    }
+}
