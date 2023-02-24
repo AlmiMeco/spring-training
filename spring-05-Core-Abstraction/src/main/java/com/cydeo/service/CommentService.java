4b482bed78da7c96e6290397eb0b5919b14f3496
Almi's PC <almimeci@gmail.com>
2023-02-24 15:07:23 -0600

    Fundamental project Pkg Structure

---
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,25 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+
+public class CommentService {
+
+    private CommentRepository dataBase;
+    private CommentNotificationProxy emailNotification;
+
+
+    public void publishComment(Comment comment){
+//    REQUIREMENTS:
+        
+        //save to DB
+        dataBase.storeComment(comment);
+        //send as email
+        emailNotification.sendComment(comment);
+
+    }
+
+
+
+}
