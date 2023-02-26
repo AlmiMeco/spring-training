bc7a535e455e2eb14d85b838b667480b217baff6
Almi's PC <almimeci@gmail.com>
2023-02-26 15:49:09 -0600

    Day 04 : Begins

---
+++ spring-07-SpringBootDemo/src/main/java/com/cydeo/proxy/BannerCommentNotificationProxy.java
@@ -0,0 +1,12 @@
+package com.cydeo.proxy;
+
+import com.cydeo.entity.Comment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BannerCommentNotificationProxy implements CommentNotificationProxy{
+    @Override
+    public void sendComment(Comment comment) {
+        System.out.println("Sending Banner Notification for comment: " + comment.getText());
+    }
+}
---
+++ spring-07-SpringBootDemo/src/main/java/com/cydeo/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Comment;
+
+public interface CommentRepository {
+    void storeComment(Comment comment);
+}
