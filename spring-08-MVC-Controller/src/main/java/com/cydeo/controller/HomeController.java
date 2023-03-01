51a3e5ca70d323fb4c0ffdd99c07779d5b4388a1
Almi's PC <almimeci@gmail.com>
2023-03-01 17:24:51 -0600

    Day 05 : Spring MVC --> "Default & Multiple End-Point" mapping

--- spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -12,4 +12,14 @@ public class HomeController {
         return "home.html";      // <-- returns .html file
     }
 
+@RequestMapping("/")          // <-- default mapping / landing page (fex: www.facebook.com/)
+    public String getDefault(){
+        return "home.html";
+    }
+
+    @RequestMapping({"/upload" , "/create"})  // <-- multiple url suffixes (/upload & /create) point -> to ONE file
+    public String getPolyEndPoint(){
+        return "home.html";
+    }
+
 }
