351d8e38046f3e59216cc77047f2695ce37d47de
Almi's PC <almimeci@gmail.com>
2023-03-01 18:25:31 -0600

    Runner added to Git

---
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/MVCDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MVCDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MVCDemoApplication.class, args);
+	}
+
+}
--- spring-08-MVC-Controller/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/controller/StudentController.java
@@ -8,7 +8,7 @@ public class StudentController {
 
     @RequestMapping("/student")
     public String getWelcomePage(){
-        return "student/welcome.html";
+        return "student/welcome.html"; // <-- must give exact location ( if not directly under "static" directory )
     }
 
 
