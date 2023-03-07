55122806429607015b8333697b2aebab08a0bc2b
Almi's PC <almimeci@gmail.com>
2023-03-07 10:32:39 -0600

    Thymeleaf DAY 08 : Begins

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -1,9 +1,21 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/mentor")
 @Controller
 public class MentorController {
 
+    @GetMapping("/register")
+    public String register(Model model){
+
+        model.addAttribute("mentor", new Mentor());
+
+        return "mentor/register";
+    }
 
 }
