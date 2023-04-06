6a65789e169007e77604bebf0bad7160370746aa
Almi's PC <almimeci@gmail.com>
2023-04-06 16:51:57 -0500

    Spring DATA DAY 16 : (Spring-DATA annotations added/explained)

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,16 +1,20 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "students")                               // <- Change tableName
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // <- primaryKey auto-generation
     private long id;
 
     private String firstName, lastName, email;
 
+    @Column(name = "maleOrFemale")                      // <- Change columnName
     private char gender;
+
+
 
 }
--- spring-11-Data-TableCreation/src/main/resources/data.sql
+++ spring-11-Data-TableCreation/src/main/resources/data.sql
@@ -2,3 +2,10 @@
 --Populating Employee Table
 INSERT INTO EMPLOYEES(id, name)
 VALUES (1, 'Mike');
+
+
+INSERT INTO students(email, first_name, gender, last_name)
+VALUES ('mike424@cydeo.com', 'Mike', 'M', 'Flash');
+
+INSERT INTO students(email, first_name, gender, last_name)
+VALUES ('jil231@cydeo.com', 'Jill', 'F', 'Jensen');
