f88aa1f88014916d612c639c1f2d43859567626d
Almi's PC <almimeci@gmail.com>
2023-04-07 14:18:02 -0500

    Spring DATA DAY 17 : BEGINS!!! ( FOREIGN KEY CREATED  )
    *FULLY WORKING*

---
+++ spring-12-Data-Relationships/src/main/java/com/cydeo/Spring12DataRelationshipsApplication.java
@@ -0,0 +1,15 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication
+public class Spring12DataRelationshipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring12DataRelationshipsApplication.class, args);
+    }
+
+}
--- spring-12-Data-Relationships/src/main/java/com/cydeo/entity/Payment.java
+++ spring-12-Data-Relationships/src/main/java/com/cydeo/entity/Payment.java
@@ -3,7 +3,6 @@ package com.cydeo.entity;
 import com.cydeo.enums.Status;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
