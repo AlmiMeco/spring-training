0570d14dc6892c94c20cc3d8e746e0e33c434df1
Almi's PC <almimeci@gmail.com>
2023-04-12 23:29:50 -0500

    Spring DATA DAY 19 : (CUSTOM QUERY CREATED)

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -26,7 +26,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 
                                             /* |CUSTOM QUERY| */
-    @Query("SELECT e.salary FROM Employee e WHERE e.firstName='Bernardine'")
+    @Query("SELECT e.salary FROM Employee e WHERE e.firstName='Bernardine'") // where 'e' represents the employee object
     Integer retrieveEmpSalary();
 
 
