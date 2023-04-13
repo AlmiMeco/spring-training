eefc25de797b695e028c05a6309657688eb7d68e
Almi's PC <almimeci@gmail.com>
2023-04-12 20:30:36 -0500

    Spring DATA DAY 19 : BEGINS!!! (Data Tables Created & popul)

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/entity/Employee.java
@@ -0,0 +1,34 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "employees")
+public class Employee extends BaseEntity {
+
+    private String firstName, lastName, email;
+
+    private LocalDate hireDate;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private Integer salary;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department")
+    private Department department;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+
+}
