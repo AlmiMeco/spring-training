9527c4466bc8b5df11ef0633af97f7ef1cef4c07
Almi's PC <almimeci@gmail.com>
2023-04-16 16:25:57 -0500

    Spring DATA DAY 20 : ( GenreRepository Query Q's DONE!!! )

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/GenreRepository.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/GenreRepository.java
@@ -2,7 +2,10 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
@@ -11,10 +14,10 @@ public interface GenreRepository extends JpaRepository<Genre, Long> {
 
     //Write a JPQL query that return all genres
 
+    @Query("SELECT g FROM Genre g")
+    List<Genre> findAll2();
 
-    // ------------------- Native QUERIES ------------------- //
 
-    //Write a native query that returns genres by containing name
 
 
 }
