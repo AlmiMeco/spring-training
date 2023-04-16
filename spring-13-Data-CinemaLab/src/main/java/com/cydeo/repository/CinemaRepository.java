9700b5c0a8a095b389bc44eb68474cce5442a1f4
Almi's PC <almimeci@gmail.com>
2023-04-16 16:24:00 -0500

    Spring DATA DAY 20 : ( CinemaRepository Query Q's DONE!!! )

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
@@ -1,8 +1,12 @@
 package com.cydeo.repository;
 
+import com.cydeo.entity.AccountDetails;
 import com.cydeo.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
@@ -11,33 +15,30 @@ public interface CinemaRepository extends JpaRepository<Cinema, Long> {
 
     //Write a derived query to get cinema with a specific name
 
+    List<Cinema> findByName(String name);
 
     //Write a derived query to read sorted the top 3 cinemas that contains a specific sponsored name
 
+    List<Cinema> findFirst3BySponsoredNameOrderBySponsoredName(String sponsoredName);
 
     //Write a derived query to list all cinemas in a specific country
 
+    List<Cinema> findAllByLocationCountry( String country);
 
     //Write a derived query to list all cinemas with a specific name or sponsored name
+
+    List<Cinema> findAllBySponsoredNameOrName(String sponsoredname, String name);
+
 
 
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query to read the cinema name with a specific id
+    @Query("SELECT c.name FROM Cinema c WHERE c.id = ?1")
+    String findCinemaNameByID(Long id);
 
 
-    // ------------------- Native QUERIES ------------------- //
 
-    //Write a native query to read all cinemas by location country
-
-
-    //Write a native query to read all cinemas by name or sponsored name contains a specific pattern
-
-
-    //Write a native query to sort all cinemas by name
-
-
-    //Write a native query to distinct all cinemas by sponsored name
 
 
 }
