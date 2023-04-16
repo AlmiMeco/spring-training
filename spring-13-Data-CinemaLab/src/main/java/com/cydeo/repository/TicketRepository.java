eceab2b255ee31541f86e2166b84f6fc8c22dca5
Almi's PC <almimeci@gmail.com>
2023-04-16 16:45:50 -0500

    Spring DATA DAY 20 : ( TicketRepository Query Q's DONE!!! )

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/TicketRepository.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/TicketRepository.java
@@ -1,8 +1,14 @@
 package com.cydeo.repository;
 
 import com.cydeo.entity.Ticket;
+import com.cydeo.entity.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
@@ -11,42 +17,30 @@ public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
     //Write a derived query to count how many tickets a user bought
 
+    Integer countDistinctByUserAccount(String user);
 
     //Write a derived query to list all tickets by specific email
 
+    List<Ticket> findAllByUserAccountEmail(String email);
 
     //Write a derived query to count how many tickets are sold for a specific movie
 
+    Integer countByMovieCinemaDateTime(LocalDateTime time);
 
     //Write a derived query to list all tickets between a range of dates
 
+    List<Ticket> findAllByDateTimeBetween(LocalDateTime startDate, LocalDateTime endDate);
 
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query that returns all tickets are bought from a specific user
 
+    @Query("SELECT t FROM Ticket t WHERE t.userAccount.username = ?1")
+    List<Ticket> findAllTicketbyUser(String userName);
 
     //Write a JPQL query that returns all tickets between a range of dates
 
-
-    // ------------------- Native QUERIES ------------------- //
-
-    //Write a native query to count the number of tickets a user bought
-
-
-    //Write a native query to count the number of tickets a user bought in a specific range of dates
-
-
-    //Write a native query to distinct all tickets by movie name
-
-
-    //Write a native query to find all tickets by user email
-
-
-    //Write a native query that returns all tickets
-
-
-    //Write a native query to list all tickets where a specific value should be containable in the username or account name or movie name
-
+    @Query("SELECT t FROM Ticket t WHERE t.dateTime BETWEEN ?1 AND ?2")
+    List<Ticket> findAllBetweenTwoDates(LocalDate date1, LocalDate date2);
 
 }
