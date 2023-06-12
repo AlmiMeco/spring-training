024d1fc8771ad1917660d583217fb8326eb123a7
Almi's PC <almimeci@gmail.com>
2023-06-12 13:46:52 -0500

    Spring AOP : [Training]
    { Creating AOP log {Before Advice} :: With More Detailed Join-info using JoinPoint param w/ {1}{2}{3}... }

--- spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
+++ spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.cydeo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -14,13 +15,33 @@ public class LoggingAspect {
     Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
 
-    @Pointcut("execution(* com.cydeo.controller.CourseController.*())")
-    public void myPointCut(){}
+//    Defining Pointcut-Expression (which methods will be intercepted by our AOP functions)
+//    @Pointcut("execution(* com.cydeo.controller.CourseController.*())")
+//    public void myPointCut(){}
 
-    @Before("myPointCut()")
-    public void log(){
-        logger.info("Info Log...............");
+//    Defining Advice (When will the interception happen && What will be the outcome)
+//    @Before("myPointCut()")
+//    public void log(){
+//        logger.info("Info Log (executing BEFORE method)...............");
+//    }
+
+//======================================================================================================================
+//======================================================================================================================
+
+                            /* Different Impl Of AOP ADVICE Functions */
+
+    @Pointcut("execution(* com.cydeo.repository.CourseRepository.findById(*))")
+    public void courseRepositoryFindByIdPC() {}
+
+    @Before("courseRepositoryFindByIdPC()")
+    public void logWithJoinInfo(JoinPoint joinPoint){
+        logger.info("Logged before method -> {}, Arguments of method -> {}, Target of method -> {}"
+        , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
     }
+//  -> Logged before method -> Optional org.springframework.data.repository.CrudRepository.findById(Object), Arguments of method -> [2], Target of method -> org.springframework.data.jpa.repository.support.SimpleJpaRepository@198a5154
+
+
+//======================================================================================================================
 
 
 }
