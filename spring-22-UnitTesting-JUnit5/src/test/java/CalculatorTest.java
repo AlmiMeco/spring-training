c4054445f3e25352a10a9591672c1ab9e8f877c4
Almi's PC <almimeci@gmail.com>
2023-06-19 23:29:38 -0500

    Spring Testing  : [DAY 39] JUnit5
    { @BeforeEach && @AfterEach annotations }

--- spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,14 +10,32 @@ class CalculatorTest {
     void add() {
     }
 
-    @Test
-    void testCase1(){
-        System.out.println("Test Case 1");
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("Method Executed Before Test Cases -> using @BeforeEach annotation");
     }
 
-    @Test
+    @AfterAll
+    static void afterAll(){
+        System.out.println("Method Executed After Test Cases -> using @AfterEach annotation");
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+
+    @Test //    Assert Equals where Test == Expected Result {PASS}
+    void testCase1(){
+        System.out.println("Test Case 1");
+        int actual = Calculator.add(1,1);
+        assertEquals(2, actual, "FAILURE!!");
+    }
+
+
+    @Test //    Assert Equals where Test Does NOT == Expected Result {FAIL}
     void testCase2(){
         System.out.println("Test Case 2");
+        int actual = Calculator.add(1,1);
+        assertEquals(4, actual, "FAILURE!!");
     }
 
     @Test
@@ -25,8 +45,8 @@ class CalculatorTest {
 
     @Test
     void testCase4(){
-//        System.out.println("Test Case 4");
-        fail("FAILURE!");
+        System.out.println("Test Case 4");
+//        fail("FAILURE!");
     }
 
 
