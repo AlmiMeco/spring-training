2805665ee7fc1819057e43c7098bfbd8f6272faf
Almi's PC <almimeci@gmail.com>
2023-06-19 23:00:36 -0500

    Spring Testing  : [DAY 39] JUnit5
    { TEST FAILURE EXAMPLE }

--- spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -7,4 +7,27 @@ class CalculatorTest {
     @Test
     void add() {
     }
+
+    @Test
+    void testCase1(){
+        System.out.println("Test Case 1");
+    }
+
+    @Test
+    void testCase2(){
+        System.out.println("Test Case 2");
+    }
+
+    @Test
+    void testCase3(){
+        System.out.println("Test Case 3");
+    }
+
+    @Test
+    void testCase4(){
+//        System.out.println("Test Case 4");
+        fail("FAILURE!");
+    }
+
+
 }
